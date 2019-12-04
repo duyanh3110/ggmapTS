85512467f52f6b00d2b88c47a03f0d87992decde
Nguyen Duy Anh <duyanh19962012@gmail.com>
2019-12-05 00:17:38 +0200

    first commit

---
+++ src/CustomMap.ts
@@ -0,0 +1,46 @@
+import { User } from './User';
+import { Company } from './Company';
+
+// Instruction to every other class
+// on  how they can be an argument to 'addMarker'
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    const mapDiv = document.getElementById(divId);
+    this.googleMap = new google.maps.Map(mapDiv, {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
